5b77ba92bb441c7256737112869c7c698073aca1
InvincibleZeal <ritesh.ganjewala17@gmail.com>
2019-03-19 23:41:16 +0530

    Added User Interface

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,6 +1,7 @@
-import { Component,  ViewChild, ElementRef } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from 'src/app/shared/services/user.service';
+import { User } from './../../shared/interfaces/user';
 
 declare var TweenMax;
 
@@ -9,34 +10,27 @@ declare var TweenMax;
     templateUrl: './register.component.html',
     styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent  {
+export class RegisterComponent {
     @ViewChild('overlay') overlay: ElementRef;
-    firstname : string;
-    lastname : string;
-    enrollment : string;
-    email : string;
-    password:string;
-    displayDuplicate : boolean;
-    gender : string = "Male";
-    phone: string;
+
+    displayDuplicate: boolean;
+    public data: User = {
+        firstName: undefined,
+        lastName: undefined,
+        email: undefined,
+        enrollment: undefined,
+        password: undefined,
+        gender: "Male",
+        phone: undefined,
+    }
 
     constructor(private userService: UserService, private router: Router) { }
 
     register(e: any) {
-        var body = {
-            firstName  : this.firstname, 
-            lastName : this.lastname, 
-            email  : this.email, 
-            enrollment : this.enrollment, 
-            password : this.password,
-            gender : this.gender,
-            phone: this.phone
-          }
-
-        this.userService.register(body).subscribe(data => {
+        this.userService.register(this.data).subscribe(data => {
             console.log(data);
             this.open()
-            if (data && data.body['email'] == this.email) {
+            if (data && data.body['email'] == this.data.email) {
                 this.open();
             }
             else {
@@ -45,14 +39,14 @@ export class RegisterComponent  {
         })
     }
 
-    open(){
+    open() {
         this.overlay.nativeElement.style.visibility = 'visible';
-        TweenMax.to(this.overlay.nativeElement, 0.2, { display : 'flex', });
-        TweenMax.to(this.overlay.nativeElement.children[1], 0.2, {  scale : 1 });
+        TweenMax.to(this.overlay.nativeElement, 0.2, { display: 'flex', });
+        TweenMax.to(this.overlay.nativeElement.children[1], 0.2, { scale: 1 });
     }
-      
-    close(){
-        TweenMax.to(this.overlay.nativeElement.children[1], 0.15, {  scale : 0 });
-        TweenMax.to(this.overlay.nativeElement, 0.15, { display : 'none', });
+
+    close() {
+        TweenMax.to(this.overlay.nativeElement.children[1], 0.15, { scale: 0 });
+        TweenMax.to(this.overlay.nativeElement, 0.15, { display: 'none', });
     }
 }
