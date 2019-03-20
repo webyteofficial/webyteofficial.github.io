e0318f63ada8281fd180c44937a70af454fa1aee
abhiarrathore <abhishekrathore246@gmail.com>
2019-03-20 16:49:36 +0530

    successfully pulled from origin and updated

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CoreModule } from '../core/core.module';
 import { SharedModule } from '../shared/shared.module';
 
-import { PAGE_ROUTES } from './pages.routing';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { EventsComponent } from './events/events.component';
